1942f0118f1f65022b521493499288b3af6d715e
Alexander ADAM <alexander.adam@vade.io>
2024-10-01 00:17:59 +0200

    updated before commit hook

--- scripts/sort_rules.sh
+++ scripts/sort_rules.sh
@@ -4,13 +4,31 @@ FILENAME="alexanderadam.txt"
 
 if git diff --cached --name-only | grep -q "$FILENAME"; then
   awk 'BEGIN { header = 1 }
-       /^[[:space:]]*$/ { header = 0 }
-       header { print; next }
-       /^[\[!]/ { print; next }
+       /^[[:space:]]*$/ { next }
+       header && /^[\[!]/ { print; next }
+       header { header = 0; print; next }
        { lines[$0] }
        END {
          n = asort(lines, sorted_lines);
          for (i = 1; i <= n; i++) print sorted_lines[i]
-       }' "$FILENAME" > temp && mv temp "$FILENAME"
-  git add "$FILENAME"
+       }' "$FILENAME" > temp
+
+  if [ -s temp ]; then
+    if grep -q "##.adsbygoogle" temp; then
+      mv temp "$FILENAME"
+      git add "$FILENAME"
+    else
+      echo "Error: temp file does not contain '##.adsbygoogle' which should make sure that rules are included. Aborting."
+      echo "Please check the file manually:"
+      echo "% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %"
+      cat temp
+      echo "% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %"
+      rm temp
+      exit 1
+    fi
+  else
+    echo "Error: temp file is empty. Aborting."
+    rm temp
+    exit 1
+  fi
 fi
